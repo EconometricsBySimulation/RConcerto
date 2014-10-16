f245a0bbb41a9872bbbd668956a54cb4840d1343
Francis Smart <francis.smart@gmail.com>
2014-10-16 17:47:29 +0200

    Update Commands.R

--- Package/Commands.R
+++ Package/Commands.R
@@ -117,7 +117,7 @@ css.get <- function(x) {
 # Twitter Bootstrap Objects
 BS <- list()
 
-BS$source <- function(theme='default') {
+(BS$source <- function(theme='default') {
   min.css <- "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css"
   theme.min.css <- "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"
   css2 <- css3 <- ''
@@ -129,8 +129,7 @@ if (theme!='default') {
   min.js <- "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"
 
 BS$SS <<- nl(min.css, theme.min.css, css2, css3, jquery.min.js, min.js)
-}
-BS$source()
+})()
 
 BS$get <- function(x)
   getURL(p("https://raw.githubusercontent.com/EconometricsBySimulation/RConcerto/master/bootstrap/",
