3904924cf9dcb4d24d128b5a0818c6da783833cf
Francis Smart <francis.smart@gmail.com>
2014-10-16 13:24:42 +0200

    Update Commands.R

--- Package/Commands.R
+++ Package/Commands.R
@@ -144,8 +144,11 @@ BS$cj <- function(title="", ..., tags=c('center','jumbotron','container'))
 BS$header <- function(...) tag$header(tag$h1(...))
 
 BS$button <- 
-  function(name="default_button", value='Click', type='default', size='', accesskey='') {
-  if (accesskey!='') accesskey <- pf(' accesskey="%s"',accesskey)
+  function(name="default_button", value='Click', type='default', size='', accesskey='', keyhint=TRUE) {
+  if (accesskey!='') {
+    if (keyhint) value <- p(value,'(',accesskey,')')
+    accesskey <- pf(' accesskey="%s"',accesskey)
+  }
   pf('\n<button type="button" name="%s" class="btn %s btn-%s"%s>%s</button>\n',
      name, size, type, value, accesskey)
   }
